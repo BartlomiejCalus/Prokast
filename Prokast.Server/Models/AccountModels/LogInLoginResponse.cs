898878481eb48dee5061595c03ddc7664f5e7d7c
HubertCiura <147997409+HubertCiura@users.noreply.github.com>
2025-10-06 19:08:54 +0200

    Add files via upload

---
+++ Prokast.Server/Models/AccountModels/LogInLoginResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Prokast.Server.Models.ResponseModels.AccountResponseModels
+{
+    public class LogInLoginResponse : Response
+    {
+        public bool IsSubscribed { get; set; }
+        public string Token { get; set; }
+    }
+}
