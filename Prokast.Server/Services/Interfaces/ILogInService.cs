e33bc66899bb154458351138980ec41c140b880c
HubertCiura <147997409+HubertCiura@users.noreply.github.com>
2025-10-06 19:05:55 +0200

    Add files via upload

--- Prokast.Server/Services/Interfaces/ILogInService.cs
+++ Prokast.Server/Services/Interfaces/ILogInService.cs
@@ -9,8 +9,8 @@ namespace Prokast.Server.Services.Interfaces
     public interface ILogInService
     {
         Response GetLogIns(int clientID);
-        string Log_In(LoginRequest loginRequest);
-        Account CreateAccount(AccountCreateDto accountCreate, int clientID);
+        Response Log_In(LoginRequest loginRequest);
+        Response CreateAccount(AccountCreateDto accountCreate, int clientID);
         Response EditAccount(AccountEditDto accountEdit, int clientID);
         Response EditPassword(AccountEditPasswordDto editPasswordDto, int clientID);
         Response DeleteAccount(int clientID, int ID);
