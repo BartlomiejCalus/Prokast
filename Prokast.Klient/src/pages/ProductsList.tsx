d71157b0deaed070533d20b26b6d9f53ceb018f5
Jacek <jacekpelikan912@gmail.com>
2025-10-12 10:32:48 +0200

    Poprawka v1

--- Prokast.Klient/src/pages/ProductsList.tsx
+++ Prokast.Klient/src/pages/ProductsList.tsx
@@ -96,11 +96,11 @@ const ProductList: React.FC = () => {
   };
 
   // Usunięcie produktu
-  const handleDelete = (title: string) => {
-    if (window.confirm(`Czy na pewno chcesz usunąć produkt: ${title}?`)) {
-        setProducts(products.filter(p => p.title !== title));
-    }
-  };
+  // const handleDelete = (title: string) => {
+  //   if (window.confirm(`Czy na pewno chcesz usunąć produkt: ${title}?`)) {
+  //       setProducts(products.filter(p => p.title !== title));
+  //   }
+  // };
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-100 via-white to-blue-200 p-4">
@@ -230,7 +230,7 @@ const ProductList: React.FC = () => {
                       Edytuj
                     </button>
                     <button
-                      onClick={() => handleDelete(product.title)}
+                      //onClick={() => handleDelete(product.title)}
                       className="px-4 py-2 bg-red-500 text-white rounded-xl hover:bg-red-600 transition"
                     >
                       Usuń
