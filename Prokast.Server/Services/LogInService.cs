893924c79b41b21c135131fe9d596c500cb7ffec
HubertCiura <147997409+HubertCiura@users.noreply.github.com>
2025-11-28 21:28:20 +0100

    Add files via upload

--- Prokast.Server/Services/LogInService.cs
+++ Prokast.Server/Services/LogInService.cs
@@ -20,6 +20,9 @@ using Prokast.Server.Models.JWT;
 using Prokast.Server.Models.ClientModels;
 using Prokast.Server.Models.ResponseModels.CustomParamsResponseModels;
 using Prokast.Server.Models.ResponseModels.RoleResponseModels;
+using static System.Net.Mime.MediaTypeNames;
+using Microsoft.Identity.Client;
+using Azure.Storage.Blobs.Models;
 
 
 
@@ -63,8 +66,8 @@ namespace Prokast.Server.Services
             var logins = _dbContext.Accounts.Where(x => x.ClientID == clientID).ToList();
             if (logins.Count() == 0)
                 return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = clientID, errorMsg = "Klient nie ma parametrów" };
-
-            return new LogInGetResponse() { ID = random.Next(1, 100000), ClientID = clientID, Model = [.. logins.Select(x => new AccountGetDto(x))] };
+            
+            return new LogInGetResponse() { ID = random.Next(1, 100000), ClientID = clientID, Model = logins };
 
         }
         #endregion
@@ -74,6 +77,7 @@ namespace Prokast.Server.Services
         {
 
             var account = _dbContext.Accounts.FirstOrDefault(x => x.Login == loginRequest.Login);
+            
             if (account == null)
                 return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = -1, errorMsg = "Nie ma takiego konta" };
 
@@ -81,6 +85,35 @@ namespace Prokast.Server.Services
             if (client == null)
                 return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = -1, errorMsg = "Błędny login" };
                 
+
+            if (account.Password != getHashed(loginRequest.Password))
+                return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = -1, errorMsg = "Błędne hasło" };
+            
+            /*if (client.Subscription is null || client.Subscription < DateTime.Now)
+            {
+                var responseFalse = new LogInLoginResponse() { ID = random.Next(1, 100000), ClientID = client.ID, IsSubscribed = false };
+                return responseFalse;
+            }*/
+            
+            var tokn = CreateToken(account).ToString();
+            
+            return new LogInLoginResponse() { ID = random.Next(1, 100000), Name = account.FirstName, Surname = account.LastName, ClientID = client.ID, IsSubscribed = true, Token = tokn };
+        }
+
+        public Response Log_In_Warehouse([FromBody] LoginRequest loginRequest)
+        {
+
+            var account = _dbContext.Accounts.FirstOrDefault(x => x.Login == loginRequest.Login);
+            if (account.RoleID != 1 && account.RoleID != 2 && account.RoleID != 3 && account.RoleID != 4)
+                return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = -1, errorMsg = "Nie masz dostępu do magazynu!" };
+
+            if (account == null)
+                return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = -1, errorMsg = "Nie ma takiego konta" };
+
+            var client = _dbContext.Clients.FirstOrDefault(x => x.Accounts.Any(y => y.ID == account.ID));
+            if (client == null)
+                return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = -1, errorMsg = "Błędny login" };
+
 
             if (account.Password != getHashed(loginRequest.Password))
                 return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = -1, errorMsg = "Błędne hasło" };
@@ -92,9 +125,11 @@ namespace Prokast.Server.Services
             }*/
             
             var tokn = CreateToken(account).ToString();
-            
-            return new LogInLoginResponse() { ID = random.Next(1, 100000), ClientID = client.ID, IsSubscribed = true, Token = tokn };
+
+            return new WarehouseLoginResponse() { ID = random.Next(1, 100000), Name = account.FirstName, Surname=account.LastName, ClientID = client.ID, IsSubscribed = true, Token = tokn, WarehouseID = account.WarehouseID };
         }
+
+
         private TokenResponseDto CreateTokenResponse(Account? user)
         {
             return new TokenResponseDto
@@ -113,7 +148,7 @@ namespace Prokast.Server.Services
         /// <param name="accountCreate"></param>
         /// <param name="clientID"></param>
         /// <returns></returns>
-        public Response CreateAccount(AccountCreateDto accountCreate, int clientID, string mail)
+        public Response CreateAccount(AccountCreateDto accountCreate, int clientID, int roleId)
         {
             const string litery = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
@@ -138,12 +173,16 @@ namespace Prokast.Server.Services
             Console.WriteLine(login);
             Console.WriteLine(password.ToString());
 
+            var role = _dbContext.Roles.FirstOrDefault(x => x.ID == roleId);
+            if (role == null)
+                return new ErrorResponse() { ID = random.Next(1, 100000), errorMsg = "Błąd przy przypisaniu roli" };
+
             var newAccount = new Account
             {
                 Login = login,
                 Password = getHashed(password.ToString()),
                 WarehouseID = accountCreate.WarehouseID,
-                RoleID = accountCreate.RoleId,
+                RoleID = role.ID,
                 FirstName = accountCreate.FirstName,
                 LastName = accountCreate.LastName,
                 ClientID = clientID
@@ -160,12 +199,12 @@ namespace Prokast.Server.Services
             var creds = new AccountCredentials()
             {
                 Login = login,
-                Password = password.ToString(),
+                Password = getHashed(password.ToString()),
             };
 
             var message = new EmailMessage
             {
-                To = [mail],
+                To = [accountCreate.Email],
                 Subject = "Dane Logowania",
                 Body = $"Login: {login}\n Hasło: {password}"
             };
@@ -180,7 +219,8 @@ namespace Prokast.Server.Services
             {
                 new Claim(ClaimTypes.Name, user.Login),
                 new Claim(ClaimTypes.NameIdentifier, user.ClientID.ToString()),
-                new Claim(ClaimTypes.Role, user.RoleID.ToString())
+                new Claim(ClaimTypes.Role, user.RoleID.ToString()),
+                new Claim(ClaimTypes.UserData, user.ID.ToString())
             };
 
             var key = new SymmetricSecurityKey(
@@ -204,17 +244,11 @@ namespace Prokast.Server.Services
         public Response EditAccount(AccountEditDto accountEdit, int clientID)
         {
             
-            var account = _dbContext.Accounts.FirstOrDefault(x => x.ID == accountEdit.Id);
+            var account = _dbContext.Accounts.FirstOrDefault(x => x.ID == clientID && x.Login == accountEdit.Login);
             if (account == null)
                 return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = clientID, errorMsg = "Błędnie podane dane" };
-
-            if (account.ClientID != clientID)
-                return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = clientID, errorMsg = "Brak uprawnień do konta" };
-
             account.WarehouseID = accountEdit.WarehouseID;
-            account.RoleID = accountEdit.RoleId;
-            account.FirstName = accountEdit.FirstName;
-            account.LastName = accountEdit.LastName;
+            account.Role = accountEdit.Role;
             _dbContext.SaveChanges();
 
             return new AccountEditResponse() { ID = random.Next(1, 100000), ClientID = clientID, Model = accountEdit };
@@ -238,6 +272,12 @@ namespace Prokast.Server.Services
         #region Delete
         public Response DeleteAccount(int clientID, int ID)
         {
+            var konto = _dbContext.Accounts.Where(x => x.ClientID == clientID && x.ID == ID).FirstOrDefault();
+            var numberOfHeadAdmins = _dbContext.Accounts.Where(x => x.ClientID == clientID && x.RoleID == 2).ToList().Count();
+
+            if (numberOfHeadAdmins == 1 && konto.RoleID == 2)
+                return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = clientID, errorMsg = "Nie możesz usunąć tego konta, ponieważ musi być co najmniej 1 HeadAdmin" };
+
             var account = _dbContext.Accounts.FirstOrDefault(x => x.ID == ID);
             if (account == null)
             {
@@ -253,7 +293,7 @@ namespace Prokast.Server.Services
         #endregion
         public Response GetAllRoles(int clientID) 
         {
-            var roleList = _dbContext.Roles.Where(x => x.RoleName != "Master" && x.RoleName != "HeadAdmin").ToList();
+            var roleList = _dbContext.Roles.ToList();
             if (roleList.Count() == 0)
                 return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = clientID, errorMsg = "Nie ma ról!" };
 
@@ -266,6 +306,30 @@ namespace Prokast.Server.Services
                 return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = clientID, errorMsg = "Nie ma takiej roli!" };
             
             return new RoleGetResponse() { ID = random.Next(1, 100000), ClientID = clientID, Model = role };
+        }
+
+        public Response EditRole(int clientID, int accountID, int newRoleID, int userRoleID) 
+        {
+            var numberOfHeadAdmins = _dbContext.Accounts.Where(x => x.ClientID == clientID && x.RoleID == 2).ToList().Count();
+            var konto = _dbContext.Accounts.Where(x => x.ClientID == clientID && x.ID == accountID).FirstOrDefault();
+            
+            if (numberOfHeadAdmins == 1 && konto.RoleID == 2)
+                return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = clientID, errorMsg = "Nie możesz zmienić roli tego konta, ponieważ musi być co najmniej 1 HeadAdmin!" };
+           
+            if(userRoleID == 3 && (konto.RoleID == 2 || konto.RoleID == 3))
+                return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = clientID, errorMsg = "Nie masz uprawnień do zmiany tych ról!" };
+            
+            if(userRoleID == 3 && (newRoleID == 2 || newRoleID == 3))
+                return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = clientID, errorMsg = "Nie masz uprawnień do nadania tych ról!" };
+
+            if(konto == null)
+                return new ErrorResponse() { ID = random.Next(1, 100000), ClientID = clientID, errorMsg = "Nie ma takiego konta!" };
+            
+            konto.RoleID = newRoleID;
+            _dbContext.SaveChanges();
+
+            return new RoleEditResponse() { ID = random.Next(1, 100000), ClientID = clientID, Name = konto.FirstName, Surname = konto.LastName, Role = konto.RoleID };
+            
         }
     }
 }
